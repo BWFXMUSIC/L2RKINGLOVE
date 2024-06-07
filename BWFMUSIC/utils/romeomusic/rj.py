8dd3adc84922fdeb2274da36dd593c20428c998e
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-06-08 02:23:35 +0530

    Delete BWFMUSIC/utils/romeomusic/rj.py

--- BWFMUSIC/utils/romeomusic/rj.py
+++
@@ -1,8 +0,0 @@
-from typing import Union, List
-from pyrogram import filters
-from config import COMMAND_PREFIXES
-
-
-
-def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
